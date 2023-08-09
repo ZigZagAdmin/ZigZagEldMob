f9f198dbd1cafa418f53651e42c4f21fb30117ea
fedoraus <ohoho.1337@gmail.com>
2023-08-09 17:54:50 +0300

    sdk version normilize

--- src/app/pages/hos/hos.page.ts
+++ src/app/pages/hos/hos.page.ts
@@ -54,18 +54,26 @@ export class HosPage implements OnInit {
     }
   }
 
+  selectButton(button: string) {
+    this.selectedButton = button;
+    console.log(this.selectedButton);
+  }
+
   cancel() {
     this.modal.dismiss(null, 'cancel');
   }
 
   confirm() {
-    this.modal.dismiss(this.name, 'confirm');
+    if (this.selectedButton) {
+      this.modal.dismiss(this.selectedButton, 'confirm');
+    }
   }
 
   onWillDismiss(event: Event) {
     const ev = event as CustomEvent<OverlayEventDetail<string>>;
     if (ev.detail.role === 'confirm') {
       this.message = `${ev.detail.data}`;
+      // Обновите кнопку Current Status
     }
   }
 
