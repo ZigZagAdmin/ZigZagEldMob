b2681fc4e81ba80c79f5b021ebf121d577bfe922
unknown <charlie.iw12@gmail.com>
2024-01-22 18:14:07 +0200

    finish with location

--- src/app/services/location.service.ts
+++ src/app/services/location.service.ts
@@ -15,18 +15,20 @@ export class LocationService {
   }
 
   async requestPermission(): Promise<void> {
-    await Geolocation.requestPermissions({ permissions: ['location', 'coarseLocation'] }).then(res => {
-      if (res.location === 'denied' || res.coarseLocation === 'denied') {
-        NativeSettings.open({
+    let res = await Geolocation.requestPermissions({ permissions: ['location', 'coarseLocation'] });
+    console.log(res);
+    if (res.location === 'denied' || res.coarseLocation === 'denied') {
+      let confirmSettings = confirm('Go to settings and grant permission.');
+      if (confirmSettings)
+        await NativeSettings.open({
           optionAndroid: AndroidSettings.ApplicationDetails,
           optionIOS: IOSSettings.App,
         });
-      }
-    });
+    }
   }
 
   async checkPermission(): Promise<boolean> {
-    console.log(await Geolocation.checkPermissions());
+    // console.log(await Geolocation.checkPermissions());
     return (await Geolocation.checkPermissions()).location === 'granted' && (await Geolocation.checkPermissions()).coarseLocation === 'granted';
   }
 }
