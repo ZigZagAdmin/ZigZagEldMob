4765558bbf96774ec042cc7bbf923a6a67166afc
Church1ll <charlie.iw12@gmail.com>
2024-02-27 14:20:45 +0200

    update android and ios

--- ios/App/App/public/4546.c8bb68e6b1c62269.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkapp=self.webpackChunkapp||[]).push([[4546],{4546:(M,a,_)=>{_.d(a,{y:()=>r});var e=_(6895),u=_(4006),l=_(7151),o=_(8256);let r=(()=>{class n{}return n.\u0275fac=function(E){return new(E||n)},n.\u0275mod=o.oAB({type:n}),n.\u0275inj=o.cJS({imports:[e.ez,u.u5,l.Pc]}),n})()}}]);
